1371c6f30dccdc0bb5ecc8eeeb8b093325eba65c
nuxarch <nuxarch@gmail.com>
2024-09-18 16:59:10 +0700

    materi while

--- Week4/helloworld.cpp
+++ Week4/helloworld.cpp
@@ -1,48 +1,22 @@
 #include <iostream>
 using namespace std;
-int bulan;
-string nama_bulan = "";
 int main(void)
 {
-    bulan = 12;
-    switch (bulan)
+    int i = 9;
+    while (i > 8)
     {
-    case 1:
-        nama_bulan = "Jan";
-        break;
-    case 2:
-        nama_bulan = "Feb";
-        break;
-    case 3:
-        nama_bulan = "Mar";
-        break;
-    case 4:
-        nama_bulan = "Apr";
-        break;
-    case 5:
-        nama_bulan = "May";
-        break;
-    case 6:
-        nama_bulan = "Jun";
-        break;
-    case 7:
-        nama_bulan = "Jul";
-        break;
-    case 8:
-        nama_bulan = "Aug";
-        break;
-    case 9:
-        nama_bulan = "Sep";
-        break;
-    case 10:
-        nama_bulan = "Okt";
-        break;
-    case 11:
-        nama_bulan = "Nop";
-        break;
-    case 12:
-        nama_bulan = "Dec";
-        break;
+        cout << "looping ke : " << i << endl;
+        --i;
+        // if (i == 0){
+        //     break;
+        // }
     }
-    cout << "sekarang bulan :" << nama_bulan << endl;
+    cout << "selesai" << endl;
+
+    int j = 0;
+    do
+    {
+        j+=2;
+        cout << "looping ke :" << j << endl;
+    } while (j < 10);
 }
