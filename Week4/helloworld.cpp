bc7b5dcd6ce71c2cb31dd3325ef8afccd7239470
nuxarch <nuxarch@gmail.com>
2024-09-18 17:04:57 +0700

    tugas

--- Week4/helloworld.cpp
+++ Week4/helloworld.cpp
@@ -1,22 +1,34 @@
+/*
+Tugas 1:
+Buat pola, sesuai no absen * 2:
+*
+**
+***
+****
+sd absen * 2
+**********
+Tugas 2:
+Buat pola, sesuai no absen * 2:
+         *
+        **
+       ***
+      ****
+sd absen * 2
+**********
+Tugas 3:
+Buat pola, sesuai no absen * 2:
+         *
+        ***
+       *****
+      *******
+   sd absen * 2
+********************
+
+gunakan branching dan looping
+*/
+
 #include <iostream>
 using namespace std;
 int main(void)
 {
-    int i = 9;
-    while (i > 8)
-    {
-        cout << "looping ke : " << i << endl;
-        --i;
-        // if (i == 0){
-        //     break;
-        // }
-    }
-    cout << "selesai" << endl;
-
-    int j = 0;
-    do
-    {
-        j+=2;
-        cout << "looping ke :" << j << endl;
-    } while (j < 10);
 }
