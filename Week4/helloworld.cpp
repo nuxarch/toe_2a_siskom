f6e95c7a61f1b6be06c66aa1ae611edd99a67ad2
nuxarch <nuxarch@gmail.com>
2024-09-18 16:34:33 +0700

    materi if else

--- Week4/helloworld.cpp
+++ Week4/helloworld.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-int a = 12;
-int b = 10;
-int hasil;
 int main(void)
 {
-    hasil = max(a, b);
-    cout << "nilai terbesar :" << hasil << endl;
-    hasil = min(a, b);
-    cout << "nilai terkecil :" << hasil << endl;
+    int a = 10;
+    int b = 20, c;
+    if (a != b)
+    {
+        c = 1;
+    }
+    if (a >= b)
+    {
+        c = 2;
+    }
+    cout << "var c:" << c << endl;
+    a = 1;
+    b = 2;
+    if (a < b)
+    {
+        c = 3;
+    }
+    else
+    {
+        c = 4;
+    }
+    cout << "var c:" << c << endl;
 
-    double x = 100;
-    cout << "akar dari " << x << ": " << sqrt(x) << endl;
-    double y = 4.67;
-    cout << "pembulatan dari " << y << ": " << round(y) << endl;
-    double z = 10;
-    cout << "log dari " << z << ": " << log10(z) << endl;
-    z = 4;
-    cout << "log dari " << z << ": " << log2(z) << endl;
-
-    double sudut = 90;
-    double radians = sudut * (3.14 / 180);
-    cout << "sinus(" << sudut << ") : " << sin(radians) << endl;
+    a = 13;
+    b = 12;
+    if (a < b)
+    {
+        c = 5;
+    }
+    else if (a == b)
+    {
+        c = 6;
+    }
+    else
+    {
+        c = 7;
+    }
+    cout << "var c:" << c << endl;
 }
