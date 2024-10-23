4d4b47a4876d1804c4e727fc352fc759ec50f6db
nuxarch <nuxarch@gmail.com>
2024-10-23 17:13:34 +0700

    dasar program arduino led blink

--- Week9/00_Latihan1/src/main.cpp
+++ Week9/00_Latihan1/src/main.cpp
@@ -1,24 +1,30 @@
 #include <Arduino.h>
 
-#define PIN_LED_KUNING  13
+#define PIN_LED_KUNING 13
 
 // dijalankan 1x
 void setup()
 {
-  // pin 13 difungsikan sbagai output
-  pinMode(PIN_LED_KUNING,OUTPUT);
+  // aktifkan komunikasi serial kec = 9600 bps
+  Serial.begin(9600);
+  Serial.println(" .......APLIKASI AKTIF.....");
 
+  // pin 13 difungsikan sbagai output
+  pinMode(PIN_LED_KUNING, OUTPUT);
 
   // '0' / false /low/0 volt/gnd
   // '1' / true /high/+5 volt/vcc
-  // set +5v pada pin 13 u/menyalakan led
-  // digitalWrite(PIN_LED_KUNING, HIGH);
-
-  // set 0v pada pin 13 u/mematikan led
-  digitalWrite(PIN_LED_KUNING, LOW);
 }
 
 // dijalankan selamanya
 void loop()
 {
+  // set +5v pada pin 13 u/menyalakan led
+  digitalWrite(PIN_LED_KUNING, HIGH);
+  Serial.println(" LED MENYALA");
+  delay(500);
+  // set 0v pada pin 13 u/mematikan led
+  digitalWrite(PIN_LED_KUNING, LOW);
+  Serial.println(" LED MATI");
+  delay(500);
 }
